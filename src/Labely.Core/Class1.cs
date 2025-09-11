ee854736d26789221587ee4d2a07f94b73eb2ef1
Jithin Johnson <jithu7432@gmail.com>
2025-09-11 22:54:06 +0530

    Draw border for canvas

--- src/Labely.Core/Class1.cs
+++ src/Labely.Core/Class1.cs
@@ -3,30 +3,36 @@ namespace Labely.Core;
 
 public static class Drawer {
     public static void Draw() {
+
+
         var info = new SKImageInfo(1200, 1800);
         using var surface = SKSurface.Create(info);
+
+        SKPaint paint;
+
         var canvas = surface.Canvas;
+        canvas.Clear(SKColors.AntiqueWhite);
+        DrawBorder(canvas, 10);
 
-        // make sure the canvas is blank
-        canvas.Clear(SKColors.Bisque);
-
-        var coord = new SKPoint(info.Width / 2, (info.Height) / 2);
-        canvas.DrawText($"{Guid.NewGuid()}", coord, SKTextAlign.Center, new SKFont(), new SKPaint());
-
-
-        var paint = new SKPaint() {
-            Color = SKColors.Black, IsStroke = true
-        };
-
-        var rect = new SKRect(
-            0, 1200, 1800, 0
-        );
-        canvas.DrawRect(rect, paint);
+        var point = new SKPoint(canvas.LocalClipBounds.Width/2, canvas.LocalClipBounds.Height/2);
+        var font = new SKFont();
+        DrawText(canvas, Guid.NewGuid().ToString(), point, new SKFont(), new SKPaint());
 
         // save the file
         using var image = surface.Snapshot();
         using var data = image.Encode(SKEncodedImageFormat.Png, 100);
         using var stream = File.OpenWrite("output.png");
         data.SaveTo(stream);
+    }
+
+    private static void DrawText(SKCanvas canvas, string text, SKPoint point, SKFont font, SKPaint paint) {
+        canvas.DrawText(text, point, SKTextAlign.Center, font, paint);
+    }
+
+    private static void DrawBorder(SKCanvas canvas, float offset) {
+        canvas.Clear(SKColors.Black);
+        var rect = new SKRect(offset, offset, canvas.LocalClipBounds.Width - offset, canvas.LocalClipBounds.Height - offset);
+        var paint = new SKPaint() { Color = SKColors.White, IsStroke = false };
+        canvas.DrawRect(rect, paint);
     }
 }
